84d7e94a6af88f5f3b3c52e523b807ab6b604d01
Sidney Arcidiacono <sidneyarci@gmail.com>
2021-04-05 16:35:00 -0700

    [update] Added pagination on pets home page.

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,16 @@
 // MODEL
 const Pet = require('../models/pet');
 
-exports.getPets = (req, res) => {
-  Pet.find().exec((err, pets) => {
-    res.render('pets-index', { pets: pets });
+exports.getPets = async (req, res) => {
+  const currentPage = req.query.page || 1
+  const perPage = 2
+  const count = await Pet.find().countDocuments()
+  pets = await Pet.find()
+    .skip((currentPage - 1) * perPage)
+    .limit(perPage)
+  res.render('pets-index', {
+    pets: pets,
+    pagesCount: count / perPage,
+    currentPage: currentPage
   });
-}
+};
